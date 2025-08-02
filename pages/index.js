4340d2f7b3eee65ad157c1e74af6191936e20b61
Clownworldenjoyer76 <mlames@protonmail.com>
2025-08-02 19:52:43 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ color: 'white', fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Sports Betting Site</h1>
+      <p>API is live. Frontend coming soon.</p>
+    </div>
+  );
+}
