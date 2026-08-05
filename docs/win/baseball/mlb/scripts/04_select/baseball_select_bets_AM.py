c3f96e0f5f57c8deeb22559d713f41f517a13b56
Clownworldenjoyer76 <mlames@protonmail.com>
2026-08-05 09:13:26 -0400

    Rename script file to baseball_select_bets_AM.py

--- docs/win/baseball/mlb/scripts/04_select/baseball_select_bets_AM.py
+++ docs/win/baseball/mlb/scripts/04_select/baseball_select_bets_AM.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# docs/win/baseball/mlb/scripts/04_select/baseball_select_bets.py
+# docs/win/baseball/mlb/scripts/04_select/baseball_select_bets_AM.py
 import traceback
 from datetime import datetime, UTC
 from pathlib import Path
